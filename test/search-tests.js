6a1c2d4f2207b64907a92f2a363d9904b98d30bc
adelaveres <adela.veres@yahoo.ro>
2017-08-30 15:55:01 +0300

    location and apartment tests added

--- test/search-tests.js
+++ test/search-tests.js
@@ -95,7 +95,7 @@ describe('Search Functionality', function() {
         console.log('');
     });
 
-    it('should check if each result has the searched-for location', () => {
+    it('should check if each property has the searched-for location (Dubai Marina)', () => {
         browser
             .url('/')
             .setValue('#tab_cat_id_field', 'Dubai Marina');
@@ -103,17 +103,86 @@ describe('Search Functionality', function() {
         browser.keys('Enter');
         browser.click('#form-submit');
         var loc_elems = browser.elements('#search_listing_section li a .right .location');
-        var loc_ids = [];
+        loc_elems = loc_elems.value;
+        var locations = [];
 
-        display_header();
         const get_ids = () => {
             for(var i=0; i< loc_elems.length; i++){
-                loc_ids.push(loc_elems[i].value.ELEMENT);
-                console.log(loc_ids[i]);
+                var loc_id = loc_elems[i].ELEMENT;
+                var loc_text = browser.elementIdText(loc_id).value;
+                locations.push(loc_text);
             }
         }
-        
-//        console.log(loc_ids);
+        const check_display_locations = () => {
+            for(var i=0; i< locations.length; i++){
+                var result_has_location = string_has_substring(locations[i],'Dubai Marina');
+                console.log(result_has_location);
+            }
+        }
+        get_ids();
+
+        display_header();
+        console.log('Locations: ');
+        console.log(locations);
+        console.log('');
+        console.log('Results have searched-for location:');
+        check_display_locations();
+    });
+
+    it('should open apartments results page when search for apartments', () => {
+        browser
+            .url('/')
+            .setValue('#tab_cat_id_field', 'Downtown Dubai')
+            .pause('2000');
+        browser
+            .keys('Enter')
+            .pause('2000');
+        browser.setValue('#for-sale-extra .prop_Type .custom-combobox input', 'Apartments');
+        browser.click('#form-submit');
+        var url = browser.getUrl();
+        var url_is_expected = string_has_substring(url,'/apartments/');
+
+        display_header();
+        console.log('Url is: ' + url);
+        console.log('Url is as expected: ' + url_is_expected);
+        console.log('');
+    });
+
+    it('should return apartments list when search for apartments', () => {
+        browser
+            .url('/')
+            .setValue('#tab_cat_id_field', 'Downtown Dubai')
+            .pause('2000');
+        browser
+            .keys('Enter')
+            .pause('2000');
+        browser.setValue('#for-sale-extra .prop_Type .custom-combobox input', 'Apartments');
+        browser.click('#form-submit');
+        var aps = browser.elements('#search_listing_section .ls-mn .block .slider_content .slider_pinfo li:nth-child(1)');
+        aps = aps.value;
+        var apartments = [];
+
+        const get_aps = () => {
+            for(var i=0; i< aps.length; i++){
+                var ap_id = aps[i].ELEMENT;
+                var ap_text = browser.elementIdText(ap_id).value;
+                apartments.push(ap_text);
+            }
+        }
+        const check_display_aps = () => {
+            for(var i=0; i< aps.length; i++){
+                var result_has_ap = string_has_substring(apartments[i],'Apartment');
+                console.log(result_has_ap);
+            }
+        }
+
+        get_aps();
+        display_header();
+        console.log('Apartments:');
+        console.log(apartments);
+        console.log('');
+        console.log('Results are apartments:');
+        check_display_aps();
     });
 
 })
